4ad29ca8940220c55c74aebb3f5e9cfd402d9f32
acharyarupak391 <acharyarupak391@gmail.com>
2021-08-08 21:16:53 +0545

    NestJS graphql API

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -17,23 +17,4 @@ export class UsersResolver {
     const _newUser = this.usersService.addUser(args);
     return _newUser;    
   }
-
-  // @Query( => Recipe)
-  // async recipe(@Args('id') id: string): Promise<Recipe> {
-  //   const recipe = await this.usersService.findOneById(id);
-  //   if (!recipe) {
-  //     throw new NotFoundException(id);
-  //   }
-  //   return recipe;
-  // }
-
-  // @Mutation( => Boolean)
-  // async removeRecipe(@Args('id') id: string) {
-  //   return this.usersService.remove(id);
-  // }
-
-  // @Subscription( => Recipe)
-  // recipeAdded() {
-  //   return pubSub.asyncIterator('recipeAdded');
-  // }
 }
